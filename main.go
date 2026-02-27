6aeacad1054bae8628b4717212d4676175279946
k <k@ip-192-168-31-137.eu-west-2.compute.internal>
2026-02-27 08:43:03 +0800

    feat: wire up main entry point with embedded frontend

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/k/tictactoe-rl/server"
+)
+
+//go:embed web/*
+var webFS embed.FS
 
 func main() {
-	fmt.Println("tictactoe-rl")
+	modelDir := "models"
+	if d := os.Getenv("MODEL_DIR"); d != "" {
+		modelDir = d
+	}
+	modelDir, _ = filepath.Abs(modelDir)
+
+	addr := ":8080"
+	if a := os.Getenv("ADDR"); a != "" {
+		addr = a
+	}
+
+	handler := server.NewHandler(modelDir)
+
+	webContent, _ := fs.Sub(webFS, "web")
+	http.Handle("/", http.FileServer(http.FS(webContent)))
+	http.HandleFunc("/ws", handler.ServeWS)
+
+	log.Printf("starting server on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
